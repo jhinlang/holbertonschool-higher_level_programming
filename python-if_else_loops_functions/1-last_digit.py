886c35b182e52beff26f8d09de29efefeed9878a
jhinlang <julienhinlang@gmail.com>
2026-01-13 10:43:50 +0000

    feat: creating a new file

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+last_digit = 0
+if last_digit > 5:
+    print(f"")
+elif last_digit == 0:
+    print(f"")
+elif last_digit < 6  && != 0:
+    print(f"")
