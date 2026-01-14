aca5f3326b19ac0107cfc0b87bc392d611de5576
jhinlang <julienhinlang@gmail.com>
2026-01-14 10:21:06 +0000

    feat: creating a function to display numbers from 1 to 100 and replace it with strings if there are multiples of specif numbers

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-i = 1
-for i in range(101):
+for i in range(1, 101):
     if i % 3 == 0 and i % 5 == 0:
-        print("FizzBuzz " end="")
+        print("FizzBuzz ", end="")
     elif i % 5 == 0:
-        print("Buzz " end="")
+        print("Buzz ", end="")
     elif i % 3 == 0:
-        print("Fizz " end="")
+        print("Fizz ", end="")
     else:
-        print("{} ".format(i) end="")
+        print("{} ".format(i), end="")
