e69f97a9324e62d16282f0663959162e8df55b73
jhinlang <julienhinlang@gmail.com>
2026-01-16 09:23:56 +0000

    correcting indentation and syntax

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,12 +5,12 @@ if __name__ == "__main__":
     argv = sys.argv
     argc = len(argv) - 1
 
-if argc == 0:
-    print("{} arguments.".format(argc)
-elif argc == 1:
-    print("{} argument:".format(argc))
-    print("1: {}".format(argv[1]))
-else:
-    print("{} arguments:".format(argc))
-    for i in range(1, len(argv))
-        print("{} {}".format(i, argv[i]))
+    if argc == 0:
+        print("{} arguments.".format(argc)
+    elif argc == 1:
+        print("{} argument:".format(argc))
+        print("1: {}".format(argv[1]))
+    else:
+        print("{} arguments:".format(argc))
+            for i in range(1, len(argv)):
+                print("{} {}".format(i, argv[i]))
