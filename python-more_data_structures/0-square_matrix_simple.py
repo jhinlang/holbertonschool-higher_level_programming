8638bf7dcadbb3e42bda7609dda16fb873adbf60
jhinlang <julienhinlang@gmail.com>
2026-01-20 08:57:14 +0000

    fix: correcting indentation errors

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-      for row in matrix:
+    for row in matrix:
         for integers in row:
             new_matrix = list(map(lambda x: x ** 2))
     return new_matrix
