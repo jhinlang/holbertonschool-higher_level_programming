b0c18b1fe35dab2794867d3904f4ea7b46be593b
jhinlang <julienhinlang@gmail.com>
2026-01-21 14:14:43 +0000

    feat: creating a function that returns a key with the biggest value

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    best_score = 0
+    best_key = ""
+    if not a_dictionary:
+        return None
+    for k, v in a_dictionary:
+        if v > best_score:
+            best_score = v
+            best_key = k
