77c6601e12ea319db2dec2d9dcb1d49d1fba13ad
jhinlang <julienhinlang@gmail.com>
2026-01-23 09:59:27 +0000

    fix: correcting syntax

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,25 +1,17 @@
 #!/usr/bin/python3
+
+
 def safe_print_list(my_list=[], x=0):
-    count = 0
-    try:
-        newList = []
-        for i in range(x):
-            print(my_list[i], end="")
-            count += 1
-    except IndexError:
-        pass
+
+    elements_number = 0
+
+    for i in range(0, x):
+        try:
+            print("{}".format(my_list[i]), end="")
+        except Exception as e:
+            break
+        else:
+            elements_number += 1
     print()
-    return count
 
-
-my_list = [1, 2, 3, 4, 5]
-
-nb_print = safe_print_list_integers(my_list, 2)
-print("nb_print: {:d}".format(nb_print))
-
-my_list = [1, 2, 3, "School", 4, 5, [1, 2, 3]]
-nb_print = safe_print_list_integers(my_list, len(my_list))
-print("nb_print: {:d}".format(nb_print))
-
-nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
-print("nb_print: {:d}".format(nb_print))
+    return elements_number
