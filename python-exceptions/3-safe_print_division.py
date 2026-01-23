8461869a9a17a2cbf5fc041c9e0b3656db4a0591
jhinlang <julienhinlang@gmail.com>
2026-01-23 09:33:42 +0000

    feat: writing a function that divide 2 integers and prints the result

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
-def safe_print_list_integers(my_list=[], x=0):
-    count = 0
-    for i in range(x):
-        try:
-
-            if isinstance(my_list[i], str):
-                continue
-
-            print("{:d}".format(my_list[i]), end="")
-            count += 1
-        except (ValueError, TypeError):
-            pass
-
-    print()
-    return count
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    result = None
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        pass
+    finally:
+        print("Inside result: {}".format(result))
+        return result
