f4aa44e2dad05360a5a76d843704bffbf3d93576
jhinlang <julienhinlang@gmail.com>
2026-01-23 08:58:15 +0000

    feat: writing a function that prints x elements of a list

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-       count = 0
+    count = 0
     try:
         newList = []
         for i in range(x):
@@ -10,6 +10,7 @@ def safe_print_list(my_list=[], x=0):
         pass
     print()
     return count
+
 
 my_list = [1, 2, 3, 4, 5]
 
