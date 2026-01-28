7b42eaf9888a6d1298e3f5eb5d7ed6568ca6e570
jhinlang <julienhinlang@gmail.com>
2026-01-28 09:24:58 +0000

    fix: adding docstring and commentaries

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -6,9 +6,9 @@ includes validation
 
 
 class Square:
-   """ Class Represents a square with size validation."""
+    """Class Represents a square with size validation."""
     def __init__(self, size=0):
-        """Initialize a Square instance
+        """Initialize a Square instance with a given size
         Args:
             size (int): The size of the square(default is 0)
             Raises:
