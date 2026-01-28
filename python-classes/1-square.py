bcd360fc88af6a90a8f26770d5d65438ceed931f
jhinlang <julienhinlang@gmail.com>
2026-01-28 08:59:53 +0000

    fix: adding docstring and commentaries

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+'''
+Module defining a Square class.
+'''
+
+
 class Square:
-    def __init__ (self, size):
+    '''Class that defines a square'''
+
+    def __init__(self, size):
+        '''Initialize a Square instance with a given size'''
         self.__size = size
