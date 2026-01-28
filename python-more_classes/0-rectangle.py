e4e774f9ad415f0af6442140f22929d2b08b4f03
jhinlang <julienhinlang@gmail.com>
2026-01-28 14:07:14 +0000

    feat: creating an empty class who defines a rectangle

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-"""Module defining a Rectangle class"""
+"""
+Module defining a Rectangle class
+"""
 
 
 class Rectangle:
-
-    """Class that defines a rectangle"""
+    """
+    Empty class that defines a rectangle
+    """
     pass
