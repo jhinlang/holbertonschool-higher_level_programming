8f06059f3a32e8b1622927c4410624afb077f052
jhinlang <julienhinlang@gmail.com>
2026-01-28 09:12:39 +0000

    fix: adding docstring and commentaries

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,25 +1,20 @@
 #!/usr/bin/python3
-'''
+"""
 This module defines a Square class with a size attribute that
 includes validation
-'''
+"""
 
 
 class Square:
-   '''
-    Represents a square with size validation.
-    Attributes:
-        __size (int): The size of the square.
-    '''
+   """ Class Represents a square with size validation."""
     def __init__(self, size=0):
-        '''
-        Initialize a Square instance with a given size
+        """Initialize a Square instance
         Args:
             size (int): The size of the square(default is 0)
             Raises:
                 TypeError: If size is not an integer.
                 ValueError: If size is negative.
-        '''
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
