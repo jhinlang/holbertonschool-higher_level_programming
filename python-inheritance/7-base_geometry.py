fee880a3dc9aab69b022bac3b9eca1568ac7c563
jhinlang <julienhinlang@gmail.com>
2026-02-04 08:53:51 +0000

    feat: adding a method to check integer

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Module that defines a BaseGeometry class with an area method"""
+
+class BaseGeometry:
+    """A base class for geometry-related classes."""
+
+    def area(self):
+        """Calculates the area of the geometry.
+
+        Raises:
+            Exception: Indicates that the area method is not implemented.
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates that a value is a positive integer.
+
+        Args:
+            name (str): The name of the variable being validated.
+            value (int): The value to validate.
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than or equal to 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
