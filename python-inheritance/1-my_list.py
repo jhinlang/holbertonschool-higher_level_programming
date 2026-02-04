77299f0f628b7cf62a306cd55c41bac2e44e348b
jhinlang <julienhinlang@gmail.com>
2026-02-04 09:15:25 +0000

    fix: correcting python style

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-"""Module that defines a MyList class inheriting from list"""
+"""
+Module that defines a class MyList that inherits from list.
+The class has a method to print the list in sorted order.
+"""
 
 
 class MyList(list):
-    """A class that inherits list with a method print a list in sorted order."""
-
+    """A class that inherits from list and has a method to print
+    the list in sorted order.
+    """
     def print_sorted(self):
-        """Prints the list in ascending sorted order."""
+        """Prints the list in sorted order."""
         print(sorted(self))
