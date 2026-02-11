f39dfe4f63fdef96bad911dccbfcdf6822527879
jhinlang <julienhinlang@gmail.com>
2026-02-11 10:33:00 +0000

    feat: writing a function that appends a string at end of file

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """
-This module contains a function that appends a string at the end of
+This module  a contains a function that appends a string at the end of
 a text file and returns the number of characters added
 """
 
