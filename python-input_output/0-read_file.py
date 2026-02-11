9d1d08573288c1fd3fd093068eea037d769e18b0
jhinlang <julienhinlang@gmail.com>
2026-02-11 10:28:03 +0000

    feat: function read and print contents of a file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,19 @@
 #!/usr/bin/python3
+
+"""
+This module contains a function for reading
+and printing the contents of a file.
+"""
+
+
 def read_file(filename=""):
-    with open('filename', encoding="utf-8") as f:
-         read_data = f.read()
+    """
+    Read and print the contents of a file.
+
+    Args:
+        filename (str): the name of the file to read.
+    """
+
+    with open(filename, encoding="utf-8") as file:
+        for line in file:
+            print(line, end="")
