4663fbf8995b208ff39fdf42f1932b227d0c2a0d
jhinlang <julienhinlang@gmail.com>
2026-03-04 15:48:25 +0100

    adding comment

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,3 +1,8 @@
-SELECT score, name
-FROM second_table
-ORDER BY score DESC;
+-- List all records in second_table ordered by score descending
+SELECT
+    score,
+    name
+FROM
+    `second_table`
+ORDER BY
+    score DESC
