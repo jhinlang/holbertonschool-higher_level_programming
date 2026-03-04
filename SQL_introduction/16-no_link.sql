1e8b36f16250a99864116c34d6162ee3e03f2349
jhinlang <julienhinlang@gmail.com>
2026-03-04 15:51:43 +0100

    Refactor SQL query with comment and formatting
    Added a comment for clarity and formatted the SQL query.

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,4 +1,10 @@
-SELECT score, name
-FROM second_table
-WHERE name IS NOT NULL AND name !=''
-ORDER BY score DESC;
+-- List records with a non-null name, ordered by score descending
+SELECT
+    score,
+    name
+FROM
+    `second_table`
+WHERE
+    name IS NOT NULL
+ORDER BY
+    score DESC
