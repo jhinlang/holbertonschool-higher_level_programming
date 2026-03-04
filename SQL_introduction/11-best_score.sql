cd02e2f99e29364383967e0b7d01d4a827c4f6b8
jhinlang <julienhinlang@gmail.com>
2026-03-04 15:49:00 +0100

    adding comment

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,4 +1,10 @@
-SELECT score, name
-FROM second_table
-WHERE score >= 10
-ORDER BY score DESC;
+-- List records with score >= 10 ordered by score descending
+SELECT
+    score,
+    name
+FROM
+    `second_table`
+WHERE
+    score >= 10
+ORDER BY
+    score DESC
