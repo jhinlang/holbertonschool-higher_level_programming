5f7f72413758f06d7859f3ce1ffffe02785ae55e
jhinlang <julienhinlang@gmail.com>
2026-03-06 09:38:08 +0000

    fix correcting syntax

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- script that creates the database and the user
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
+
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
