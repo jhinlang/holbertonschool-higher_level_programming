24a1cc107401d9579f9003ee6867b0f5b1ff9fa4
jhinlang <julienhinlang@gmail.com>
2026-03-23 10:13:17 +0000

    feat: writing a script that change the color on click

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+
+document.querySelector("#red_header").addEventListener("click", function(){
+    document.querySelector.style.color = "#ff0000";
+});
---
+++ javascript-dom_manipulation/README.md
@@ -0,0 +1,1 @@
+This directory is dedicated explore deeper js and manipulate DOM
